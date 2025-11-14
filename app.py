1905aa407b6c723f45e70b0ca5bb0c9a23ca9534
devmudassir108345 <devmudassir5@gmail.com>
2025-11-14 22:20:37 +0500

    new cod

--- app.py
+++ app.py
@@ -2103,4 +2103,14 @@ async def internal_error_handler(request: Request, exc):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=port,
+        reload=False
+    )
+
+
